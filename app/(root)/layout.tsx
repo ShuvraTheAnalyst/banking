8c041f756ad13a491b3eb6736eda83f1d308826e
Shuvra Roy <shuvraroy88@gmail.com>
2024-06-23 18:51:21 +0530

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Shuvra', lastName: 'Roy'};
   return (
-    <main>
-        SIDEBAR
-        {children}
-    </main>
+    <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn} />
+
+    <div className="flex size-full flex-col">
+      <div className="root-layout">
+        <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+        <div>
+          <MobileNav user={loggedIn} />
+        </div>
+      </div>
+      {children}
+    </div>
+  </main>
   );
 }
